e2e3e44484187fab8ff24c5271635997afcc865c
Francisco Italo Silva do Nascimento <italo.silvan87@gmail.com>
2021-12-14 15:18:44 -0300

    problema na matriz

--- tra1/sequencial.py
+++ tra1/sequencial.py
@@ -1,4 +1,4 @@
-
+# import numpy
 
 def createMatrz(file):
     row = 0
@@ -18,11 +18,35 @@ def printMatriz(size,matriz):
         print(matriz[a])  
 
 
-def multMatriz():
+def multMatriz(m,n,p):
+    # In [2]:a =  np.zeros((4,4), dtype=np.float64)
     A = createMatrz("tra1/entradas/A4x4.txt")
     B = createMatrz("tra1/entradas/B4x4.txt")
-    printMatriz(4,A);
-    
+    row = [0]*4
+    colum = [0]*4;
+    C = [row]*4;
+
+    printMatriz(4,a);
+    print("==========")
+    printMatriz(4,B);
+    i = 0;
+    j =0;
+    k = 0;
+
+    for i in range(m):
+        for j in range(n):
+            soma= 0;
+            for k in range(p):
+                soma +=  (int(A[i][k]) * int(B[k][j]));
+
+            C[i][j] = soma;
+            
+            
+
+    print();
+    printMatriz(len(C),C);
 
 
-multMatriz()
+
+multMatriz(4,4,4)
+
