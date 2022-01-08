f489cb2b4334677eca00e76be6e7df4b8e14db65
Epitácio Bessa da Silva <bessa.epitacio@gmail.com>
2022-01-08 16:19:44 -0300

    Read

--- trabalho_2/main.py
+++ trabalho_2/main.py
@@ -14,24 +14,29 @@ class LinkedList():
         self.head = head
 
 
-class ReadThread(Thread):
-    def __init__(self, linked_list):
-        Thread.__init__(self)
-        self.linked_list = linked_list
-
-    def run(self):
-        print("Thread Read, delay 3")
-        sleep(self.delay)
-        print(linked_list.value)
-        print("Read Finished")
-
-
-class InsertThread(Thread):
+class CommonThread(Thread):
     def __init__(self, linked_list, value):
         Thread.__init__(self)
         self.linked_list = linked_list
         self.value = value
 
+
+class ReadThread(CommonThread):
+    def run(self):
+        ll = self.linked_list.head
+        cont = 0
+
+        while ll != None:
+            cont += 1
+            if ll.value == self.value:
+                break
+            ll = ll.next
+
+        if ll != None:
+            print(f'Valor ({ll.value}) encontrado no nó {cont}!')    
+
+
+class InsertThread(CommonThread):
     def run(self):
         ll = self.linked_list.head
 
@@ -46,12 +51,7 @@ class InsertThread(Thread):
             ll.next = temp
 
 
-class RemoveThread(Thread):
-    def __init__(self, linked_list, value):
-        Thread.__init__(self)
-        self.linked_list = linked_list
-        self.value = value
-
+class RemoveThread(CommonThread):
     def run(self):
         ll = self.linked_list.head
 
@@ -81,19 +81,21 @@ if __name__ == '__main__':
     for i in range(0, 10):
         t = InsertThread(linked_list, i)
         t.start()
+    
     t1 = InsertThread(linked_list, 10)
     t2 = RemoveThread(linked_list, 1)
+    t3 = ReadThread(linked_list, 0)
 
     t1.start()
     t2.start()
+    t3.start()
 
     t1.join()
     t2.join()
+    t3.join()
 
     ll = linked_list.head
 
     while ll != None:
         print(ll.value)
         ll = ll.next
-    
-    del linked_list
