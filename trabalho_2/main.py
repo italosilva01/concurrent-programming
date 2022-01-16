c479dd5a557b27ff72bcd96221d548d5f5f31a86
Epitácio Bessa da Silva <bessa.epitacio@gmail.com>
2022-01-16 17:57:22 -0300

    Refatoraçao

--- trabalho_2/main.py
+++ trabalho_2/main.py
@@ -2,9 +2,9 @@ from threading import Thread,Lock,Condition
 import os
 from time import sleep
 
-lock = Lock();
-lockCond = Lock();
-cond = Condition(lockCond);
+lock = Lock()
+lockCond = Lock()
+cond = Condition(lockCond)
 
 class Node():
     def __init__(self, value = None, next = None, previous = None):
@@ -24,26 +24,6 @@ class CommonThread(Thread):
         self.linked_list = linked_list
         self.value = value
 
-def removeItem(self):
-    ll = self.linked_list.head
-    while ll != None:
-        if ll.value == self.value:
-            break
-        ll = ll.next
-    
-    if ll != None:
-        if ll.next != None:
-            ll.next.previous = ll.previous
-
-        # caso tenho um artior conect com o próximo
-        # caso não tenha um anterior, ou seja, é a cabeça, então passa a cabeça da lista pro próximo
-        # caso só tenha a cabeça então cria um nó vazio
-        if ll.previous != None:
-            ll.previous.next = ll.next
-        elif ll.next != None:
-            self.linked_list.head = ll.next
-        else:
-            self.linked_list.head = Node()
 
 class ReadThread(CommonThread):
     def run(self):
@@ -62,10 +42,6 @@ class ReadThread(CommonThread):
        
         cond.notify()
         cond.release()
-        
-        
-        
-
 
 
 class InsertThread(CommonThread):
@@ -78,7 +54,7 @@ class InsertThread(CommonThread):
         while ll.next != None:
             ll = ll.next
 
-        # Inserir na cabeça da lista
+        # inserir na cabeça da lista, caso ainda não tenha nenhum nó com valor inserido
         if ll.value == None:
             ll.value = self.value
         else:
@@ -91,39 +67,65 @@ class InsertThread(CommonThread):
 
 
 class RemoveThread(CommonThread):
+    def removeItem(self):
+        ll = self.linked_list.head
+        while ll != None:
+            if ll.value == self.value:
+                # casjo seja a cabeça
+                if ll.previous == None:
+                    # caso só tenha a cabeça, e precise removê-la
+                    if ll.next == None:
+                        self.linked_list.head = Node()
+                    else: 
+                        # caso tenha um próximo, 
+                        # o próximo se tornará a cabeça
+                        self.linked_list.head = ll.next
+                else:
+                    # caso seja o último da lista, o next do anterior deve ser nulo
+                    if ll.next == None:
+                        ll.previous.next = None
+                    else:
+                        # caso tenha um previous e um next, conecta os dois
+                        ll.previous.next = ll.next
+                        ll.next.previous = ll.previous
+
+                # removendo o nó da memória
+                del ll
+                # encerrando a removação
+                break
+            ll = ll.next
+
     def run(self):   
-          
-        if(lockCond.locked()==True):
+        if(lockCond.locked() == True):
             cond.acquire()
-            condition = cond.wait(10)    
-      
-        if lockCond.locked()==False:
+            condition = cond.wait(5)    
+
+        if lockCond.locked() == False:
             print('Removendo')           
             lock.acquire() 
-            removeItem(self)
+            self.removeItem()
             lock.release() 
-
         else:
             print('Removendo')           
             if condition:
-              
-                removeItem(self)
+                self.removeItem()
             else:
                 print("waiting timeout...")
             cond.notify()
             cond.release()
-        
 
 
 if __name__ == '__main__':
+    # Criando a lista
     linked_list = LinkedList(Node())
 
+    # inserindo 10 números na lista
     for i in range(0, 10):
         t = InsertThread(linked_list, i)
         t.start()
-    
+
     os.system('clear')
-    
+
     t1 = InsertThread(linked_list, 10)
     t3 = ReadThread(linked_list, 0) 
     t4 = ReadThread(linked_list, 1)
@@ -131,7 +133,6 @@ if __name__ == '__main__':
     t2 = RemoveThread(linked_list, 1)
     t6 = RemoveThread(linked_list, 7)
 
-  
     t3.start()
     t4.start()
     t5.start()
